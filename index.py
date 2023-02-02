39e321b1aa518f74cb044ee5fc3b23bcf39ab9aa
Csenki Gergely <118981063+CsenkiGergely@users.noreply.github.com>
2023-02-02 13:11:49 +0100

    harc

---
+++ index.py
@@ -0,0 +1,43 @@
+import pygame
+
+class Harcos:
+  def __init__(self, nev, eletero, harciero):
+    self.nev = nev
+    self.eletero = eletero
+    self.harciero = harciero
+
+  def getEletero(self):
+    return self.eletero
+
+  def getHarciero(self):
+    return self.harciero
+
+  def sebzodik(self, harciero):
+    self.eletero -= harciero
+
+  def harcol(self, harcos):
+    self.sebzodik(harcos.getHarciero())
+    harcos.sebzodik(self.getHarciero())
+    if self.getEletero() < 1 or harcos.getEletero() < 1:
+      return True
+    return False
+
+  def __repr__(self):
+    return f'<object.harcos: {self.nev}, HE:{self.getHarciero()}, EE:{self.getEletero()})>'
+
+    h1 = Harcos('Spartakus', 200, 30)
+h2 = Harcos('Herkules', 120, 40)
+
+kor = 1
+while not h1.harcol(h2):
+  print(f'{kor}. kör:')
+  print(h1)
+  print(h2)
+  kor+=1
+
+if h1.getEletero()<1 and h2.getEletero()<1:
+  print('Mindketten veszítettel!')
+elif h1.getEletero() < 1:
+  print(f'Nyertes: {h2}')
+else:
+  print(f'Nyertes: {h2}')
